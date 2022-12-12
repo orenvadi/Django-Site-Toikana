56345f510cd733f931e3fec3e99e307d95cd85ad
Aikanysh <lailievaa02@gmail.com>
2022-12-12 21:57:27 +0600

    added custom user model,registration,login

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    phone=models.CharField(blank=True,null=True,max_length=100)
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView,register
 
 urlpatterns = [
-path("signup/", SignUpView.as_view(), name="signup"),
+path("signup/", register, name="signup"),
 ]
