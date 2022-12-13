7eca993bf2cf35a6eb45c0d3eb86ce2c797bde12
hydroxygenium <orenvady@gmail.com>
2022-12-13 20:15:42 +0600

    connected login, frontend optimized

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 
 class CustomUser(AbstractUser):
     is_admin = models.BooleanField(default=False)
-    phone=models.CharField(blank=True,null=True,max_length=100)
+    phone = models.CharField(blank=True, null=True, max_length=100)
